72cb2613c4ff08e1520ab145fad2eeec90750d84
nabilbaya88 <nabilbaya514@gmail.com>
2026-09-23 14:25:00 +0200

    Fix base path configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-base: '/vtc-paris-test/',
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/vtc-paris-test/',
   server: {
     host: "0.0.0.0",
     port: 3000,
